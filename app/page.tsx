c9961dc43fd4ff04fa3383f1c400576fadfd9cde
nghiapt-comatek <100978931+jenazu@users.noreply.github.com>
2022-12-24 11:00:57 +0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
+
 import React from 'react'
 import { categories } from '../constants'
 import fetchNews from '../lib/fetchNews'
+import NewsList from './NewsList'
+
 
 async function Homepage() {
 
   // fetch the news data 
   const news: NewsResponse = await fetchNews(categories.join(","))
 
-  console.log('aq',news)
-
   return (
     <div>
       {/* NewsList news */}
-      dd
+      <NewsList news={news}/>
     </div>
   )
 }
